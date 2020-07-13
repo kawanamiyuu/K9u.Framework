7ff95eaef511ed04545df9c9d3943589d124444c
kawanamiyuu <i.am.really.you@gmail.com>
2020-07-13 23:40:02 +0900

    inject StreamFactory

--- demo/src/FakeRequestHandler.php
+++ demo/src/FakeRequestHandler.php
@@ -7,21 +7,27 @@ namespace K9u\Framework;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 class FakeRequestHandler implements RequestHandlerInterface
 {
     private ResponseFactoryInterface $responseFactory;
 
-    public function __construct(ResponseFactoryInterface $responseFactory)
+    private StreamFactoryInterface $streamFactory;
+
+    public function __construct(ResponseFactoryInterface $responseFactory, StreamFactoryInterface $streamFactory)
     {
         $this->responseFactory = $responseFactory;
+        $this->streamFactory = $streamFactory;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         unset($request); // unused
 
-        return $this->responseFactory->createResponse(200);
+        return $this->responseFactory->createResponse(200)
+            ->withHeader('Content-Type', 'application/json; charset=utf-8')
+            ->withBody($this->streamFactory->createStream('{"greeting": "Hello world !"}'));
     }
 }
