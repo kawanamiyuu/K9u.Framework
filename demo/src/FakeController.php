38f32c33cd5adffcf6747c8777d959127f388dc0
kawanamiyuu <i.am.really.you@gmail.com>
2020-07-20 21:35:12 +0900

    feat: resolve ServerRequestInterface for handler method argument

--- demo/src/FakeController.php
+++ demo/src/FakeController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace K9u\Framework\Demo;
 
 use K9u\RequestMapper\Annotation\GetMapping;
+use K9u\RequestMapper\PathParams;
+use Psr\Http\Message\ServerRequestInterface;
 
 class FakeController
 {
@@ -26,5 +28,20 @@ class FakeController
     public function getBlogs(): array
     {
         return ['path' => '/blogs'];
+    }
+
+    /**
+     * @GetMapping("/blogs/{id}")
+     *
+     * @param ServerRequestInterface $request
+     *
+     * @return array<string, mixed>
+     */
+    public function getBlogById(ServerRequestInterface $request): array
+    {
+        $pathParams = $request->getAttribute(PathParams::class);
+        assert($pathParams instanceof PathParams);
+
+        return ['path' => "/blogs/{$pathParams['id']}"];
     }
 }
--- src/HandlerMethodArgumentsResolver.php
+++ src/HandlerMethodArgumentsResolver.php
@@ -8,16 +8,26 @@ use K9u\RequestMapper\HandlerMethodArgumentsResolverInterface;
 use K9u\RequestMapper\PathParams;
 use Psr\Http\Message\ServerRequestInterface;
 use ReflectionMethod;
+use ReflectionNamedType;
 
 class HandlerMethodArgumentsResolver implements HandlerMethodArgumentsResolverInterface
 {
     public function __invoke(ReflectionMethod $method, ServerRequestInterface $request, PathParams $pathParams): array
     {
-        unset($request); // unused
-        unset($pathParams); // unused
+        $request = $request->withAttribute(PathParams::class, $pathParams);
 
         $args = [];
         foreach ($method->getParameters() as $param) {
+            if ($param->hasType()) {
+                $type = $param->getType();
+                assert($type instanceof ReflectionNamedType);
+
+                if (is_a($type->getName(), ServerRequestInterface::class, true)) {
+                    $args[] = $request;
+                    continue;
+                }
+            }
+
             // TODO: assign Path params, Query params, Parsed body, ...
             $args[] = $param->isDefaultValueAvailable() ? $param->getDefaultValue() : null;
         }
